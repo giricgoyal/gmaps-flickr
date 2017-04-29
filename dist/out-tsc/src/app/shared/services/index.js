c3ddcb09d4ab25b113b25da1287e75caf1d2d8e0
giricgoyal <giricgoyal@gmail.com>
2017-04-29 15:10:19 +0530

    remove dist & node_modules

--- dist/out-tsc/src/app/shared/services/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./http.service"));
-__export(require("./toaster.service"));
-__export(require("./session.service"));
-//# sourceMappingURL=index.js.map
